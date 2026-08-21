ce5e476083f65a2c3fdf54980f077159478c1c98
Madhav Dhir <madhavdhirrockstar@gmail.com>
2026-08-21 15:13:11 +0530

    backend added

---
+++ backend/controllers/productController.js
@@ -0,0 +1,40 @@
+const Product = require("../models/Product");
+
+// GET all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// CREATE product
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getProducts,
+  createProduct,
+};
